0caabf562458231b521657050c9ca9134a9db815
Verma <akash.verma@solera.com>
2022-11-07 14:58:03 +0530

    FeatureOne

---
+++ MyFirstProject/MyFirstProject/day1.examples/FeatureOne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.day1.examples
+{
+    class FeatureOne
+    {
+        static void Main()
+        {
+            Console.WriteLine("Akash");
+        }
+    }
+}
